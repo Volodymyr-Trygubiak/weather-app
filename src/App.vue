d8539fdddee34c391031314c385665ad9f202962
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-05-12 14:22:42 +0300

    fixed create problem

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";  
+export default {
+  name: "App",
+  data() {
+    return {
+      APIkey: "de3ff7e4a2e6e2ae885cc072dc222bc2",
+      city: "Ivano-Frankivsk"
+    }
+  },
+  methods: {
+    getCurrentWeather() {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${this.APIkey}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+</style>
