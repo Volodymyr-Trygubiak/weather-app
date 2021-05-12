79fdd4a21032a2cc955cde997bdf460aa0c72fef
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-05-12 13:56:35 +0300

    Create weather app proj

---
+++ weather-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+</style>
